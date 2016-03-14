6735f0a71047c039fd28b6a89de6063cdea30af4
BongiZithelo <tunkyb@gmail.com>
2016-03-14 09:23:08 +0200

    Changed Forgot Password

--- app/src/main/java/example/com/timesheet/login.java
+++ app/src/main/java/example/com/timesheet/login.java
@@ -5,9 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
 Button button;
+TextView txtPassword, txtUserName, tvForgotPassword;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,10 +20,25 @@ Button button;
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(login.this, InternHomePage.class);
+               // if( "Bongi".equals(txtUserName) &&  "123".equals(txtPassword))
+               // {
+                     intent = new Intent(login.this, InternHomePage.class);
+                    startActivity(intent);
+              //  }
+              //  else
+              //  {
+                    intent = new Intent(login.this,InternHomePage.class);
+                }
+
+        });
+
+tvForgotPassword = (TextView) findViewById(R.id.forgot_password);
+        tvForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent (login.this, ForgotPassword.class);
                 startActivity(intent);
             }
-
         });
 
 
