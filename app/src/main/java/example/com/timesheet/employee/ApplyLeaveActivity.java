34d4f42b79cb8a964f86bb629130989d489a98a9
crywizzy <crywizzy@gmail.com>
2016-03-29 16:03:44 +0200

    added code for adapter

--- app/src/main/java/example/com/timesheet/employee/ApplyLeaveActivity.java
+++ app/src/main/java/example/com/timesheet/employee/ApplyLeaveActivity.java
@@ -2,15 +2,43 @@ package example.com.timesheet.employee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import example.com.timesheet.R;
 
 
 public class ApplyLeaveActivity extends AppCompatActivity {
+    String[] leave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apply_leave);
+
+        leave =
+                getResources().getStringArray(R.array.leave_array);
+
+        Spinner s1 = (Spinner) findViewById(R.id.leave_spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, leave);
+        s1.setAdapter(adapter);
+        s1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> arg0,
+                                       View arg1, int arg2, long arg3)
+            {
+                int index = arg0.getSelectedItemPosition();
+                Toast.makeText(getBaseContext(),
+                        "You have selected item : " + leave[index],
+                        Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) { }
+        });
     }
 }
