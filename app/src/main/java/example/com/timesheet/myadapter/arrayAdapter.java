af28917424f33a45006c5c32cbce9bf4e7e407a4
SogaLempe <lempesoga@gmail.com>
2016-04-25 14:58:59 +0200

    Update of app and change to the xml activities

---
+++ app/src/main/java/example/com/timesheet/myadapter/arrayAdapter.java
@@ -0,0 +1,13 @@
+package example.com.timesheet.myadapter;
+
+import android.widget.ArrayAdapter;
+
+import example.com.timesheet.R;
+
+/**
+ * Created by codet on 4/22/2016.
+ */
+public class arrayAdapter {
+
+
+}
