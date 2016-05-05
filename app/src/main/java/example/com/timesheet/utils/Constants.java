d97303db562060da6d64b9f9b9040d8d7d47efd9
Hilton <hiltira@gmail.com>
2016-05-05 11:58:23 +0200

    Added Constants class

--- app/src/main/java/example/com/timesheet/utils/Constants.java
+++ app/src/main/java/example/com/timesheet/utils/Constants.java
@@ -7,6 +7,8 @@ public class Constants {
     public static String loginRequestType = "104";
     public static String requestType = "requestType";
     public static String email = "email";
+
+    public static String name = "email";
     public static String password = "password";
 
     public static String helpDeskUrl = "http://10.50.129.68:8080/TimeApp/ms3";
