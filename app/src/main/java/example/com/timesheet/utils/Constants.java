bb610ddd7e7aa35442d547dd817b6d80c6b369e2
Hilton <hiltira@gmail.com>
2016-05-05 11:25:35 +0200

    Added OkHttp Class and library

---
+++ app/src/main/java/example/com/timesheet/utils/Constants.java
@@ -0,0 +1,13 @@
+package example.com.timesheet.utils;
+
+/**
+ * Created by User on 5/4/2016.
+ */
+public class Constants {
+    public static String loginRequestType = "104";
+    public static String requestType = "requestType";
+    public static String email = "email";
+    public static String password = "password";
+
+    public static String helpDeskUrl = "http://10.50.129.68:8080/TimeApp/ms3";
+}
