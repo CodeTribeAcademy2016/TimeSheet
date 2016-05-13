09ea1d09f646480bce6dbf2220669d9fd4a7cf13
Mpjane Sello <crywizzy@gmail.com>
2016-05-13 12:07:06 +0200

    new code for splash.java

--- app/src/main/java/example/com/timesheet/Splash.java
+++ app/src/main/java/example/com/timesheet/Splash.java
@@ -1,63 +1,41 @@
 package example.com.timesheet;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
+
 
 public class Splash extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        final ImageView iv = (ImageView) findViewById(R.id.imageView1);
-        //final Animation an = AnimationUtils.loadAnimation(getBaseContext(),R.anim.rotate);
-        // final Animation an2 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.rotate);
-
-        // Animation fadeIn = new AlphaAnimation(0,1);
-        // fadeIn.setInterpolator(new DecelerateInterpolator());
-        // fadeIn.setDuration(1000);
-
-        //Animation fadeOut = new AlphaAnimation(1,0);
-        // fadeIn.setInterpolator(new DecelerateInterpolator());
-        // fadeOut.setStartOffset(1000);
-        //fadeIn.setDuration(1000);
-
-        // AnimationSet animation = new AnimationSet(false); //change to false
-        // animation.addAnimation(fadeIn);
-        // animation.addAnimation(fadeOut);
-        //iv.setAnimation(animation);
-
-
-        //ImageView myView = (ImageView)splashDaialog.findViewById(R.id.imageView);
-
-        ObjectAnimator fadeOut = ObjectAnimator.ofFloat(iv, "alpha", 1f, .3f);
-        fadeOut.setDuration(2000);
-        ObjectAnimator fadeIn = ObjectAnimator.ofFloat(iv, "alpha", .3f, 1f);
-        fadeIn.setDuration(2000);
-
-        final AnimatorSet mAnimationSet = new AnimatorSet();
-
-        mAnimationSet.play(fadeIn).after(fadeOut);
-
-        mAnimationSet.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                super.onAnimationEnd(animation);
-                mAnimationSet.start();
-
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(Splash.this, LoginActivity.class);
+                    startActivity(intent);
+                }
             }
-
-
-        });
-       // mAnimationSet.start();
-
-
+        };
+        timerThread.start();
     }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
 }
