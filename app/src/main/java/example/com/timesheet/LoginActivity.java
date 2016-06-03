9ad6a6763d23a03ce68f2bac2e5d6bd3181f348e
Hilton <hiltira@gmail.com>
2016-06-03 13:33:36 +0200

    getemployeeList

--- app/src/main/java/example/com/timesheet/LoginActivity.java
+++ app/src/main/java/example/com/timesheet/LoginActivity.java
@@ -1,23 +1,26 @@
 package example.com.timesheet;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import example.com.timesheet.employee.InternHomeActivity;
+import example.com.timesheet.utils.Constants;
+import example.com.timesheet.utils.TimeOkHttp;
 
 //import example.com.timesheet.utils.Constants;
 //import example.com.timesheet.utils.TimeOkHttp;
 
 public class LoginActivity extends AppCompatActivity {
-
+    public static String LOG_TAG = LoginActivity.class.getSimpleName();
     ///final OkHttpClient okHttpClient = new OkHttpClient();
     Button button;
     TextView txtPassword, txtUserName, tvForgotPassword;
@@ -49,6 +52,60 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        this.getEmployeeList();
+
+
+    }
+
+    public void getEmployeeList() {
+
+        String lerato = "";
+        try {
+            lerato = new JSONObject()
+                    .put(Constants.requestType,Constants.GET_EMP_LIST)
+                    .put(Constants.companyID,"1")
+                    .toString();
+        } catch (JSONException e) {
+            Log.e("Ygritte = "+LOG_TAG, e.getMessage());
+        }
+
+        Log.i("Ygritte - Payload ", lerato);
+        final TimeOkHttp timeOkHttp = new TimeOkHttp();
+
+        final String url = Constants.TimeSheetEmpUrl;
+        final String vero = lerato;
+
+            Thread thread = new Thread(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    try
+                    {
+                        //Your code goes here
+                        String response = timeOkHttp.post(url + "?JSON=" + vero, vero);
+                        Log.i("Ygritte - Response ", response);
+
+                        JSONObject res = new JSONObject(response);
+                        JSONArray employes = res.getJSONArray("employeelist");
+
+                        for ( int i = 0; i < employes.length(); i++) {
+                            int emp_no = i+1;
+                            JSONObject employee = employes.getJSONObject(i);
+                            Log.i("Ygritte", employee.getString("empName"));
+                            Log.i("Ygritte - Emp : "+emp_no, employes.get(i).toString());
+                        }
+
+                    }
+                    catch (Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            thread.start();
 
 
     }
